de0bdfdde9868ef30015a307d7e6535f2227ef2d
Lucrezia Carena <lucrezia.carena@gmail.com>
2026-01-21 16:42:15 +0100

    implement "Complete" on web

--- Task_Manager_App/web.py
+++ Task_Manager_App/web.py
@@ -13,10 +13,15 @@ st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app will increase your productivity")
 
-for todo in todos: 
-    st.checkbox(todo)
-
+for index,todo in enumerate(todos): 
+    checkbox =st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        write_todo(todos)
+        del st.session_state[todo]
+        st.rerun()
+        
+        
 st.text_input(label= "" ,placeholder="Add a new Todo...",
               on_change=add_todo, key="new_todo")
 
-print('hello')
